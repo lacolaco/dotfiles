9b6f8d8ab82893f0248617abbb2ba2e14efb8a5f
laco0416 <laco0416+github@gmail.com>
2017-07-20 15:23:08 +0900

    update

--- brew.sh
+++ brew.sh
@@ -1,22 +1,29 @@
 #! /bin/sh
 
+echo "=== Install Fish Shell"
+
 brew install fish
 sudo sh -c "echo '/usr/local/bin/fish' >> /etc/shells"
 chsh -s /usr/local/bin/fish
 
 # fisherman (fish plugin manager)
+echo "=== Install Fisherman"
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 fisher
+fisher up
+
+echo "=== Install Homebrew packages"
 
 brew install tree
 brew install curl
 brew install wget
 brew install git
 brew install nodebrew
+brew install rbenv
+brew install nodenv
 
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
-brew cask install google-chrome-canary
 brew cask install google-japanese-ime
 brew cask install sourcetree
 brew cask install dropbox
--- setup.sh
+++ setup.sh
@@ -3,19 +3,34 @@
 cp -R .gitconfig ~/
 cp -R ./fish ~/.config
 
+# for Sierra
+touch ~/.ssh/config
+SSH_CONFIG=`cat ./ssh/config`
+echo "$SSH_CONFIG" > ~/.ssh/config
+
+echo "=== Generate SSH key"
+
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub | pbcopy
+echo "Register your SSH key for GitHub"
+open "https://github.com/settings/keys"
+read -p "Press any key after finished."
+
 # Homebrew
+echo "=== Install Homebrew"
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ./brew.sh
 
-# node
+brew doctor
+
+# nodebrew
+echo "=== Install Nodebrew"
 nodebrew install-binary 6
 nodebrew use 6
 
-# 非公開ファイルの表示
+echo "=== Setup Mac environment"
 defaults write com.apple.finder AppleShowAllFiles TRUE
 killall Finder
-
-ssh-keygen
-cat ~/.ssh/id_rsa.pub | pbcopy
-# ssh公開鍵をいろいろなところに配る
